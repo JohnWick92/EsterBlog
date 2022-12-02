31f66dd1ef744116c278eb18227b03fa0f7af81e
JohnWick92 <grumec92@gmail.com>
2022-12-01 21:22:19 -0300

    no more need of signUp, so removed

--- api/src/Routes.ts
+++ api/src/Routes.ts
@@ -1,7 +1,6 @@
 import { Request, Response, Router } from 'express'
 
 import AuthBloggerControl from './controllers/Blogger/Auth'
-import SignUpBloggerControl from './controllers/Blogger/SignUp'
 import SignInBloggerControl from './controllers/Blogger/SignIn'
 
 import CreatePostController from './controllers/Post/Create'
@@ -9,7 +8,6 @@ import CreatePostController from './controllers/Post/Create'
 const router = Router()
 
 const authBloggerControl = new AuthBloggerControl()
-const signUpBloggerControl = new SignUpBloggerControl()
 const signInBloggerControl = new SignInBloggerControl()
 
 const createPostController = new CreatePostController()
@@ -18,7 +16,6 @@ router.get('/', (request: Request, response: Response) => {
   response.json('Ready :) ^-^')
 })
 
-router.post('/blogger/signUp', signUpBloggerControl.handle)
 router.post('/blogger/signIn', signInBloggerControl.handle)
 router.post('/blogger/auth', authBloggerControl.handle)
 
