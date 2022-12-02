e48e8089abb472d0166feae1ddf63a73e0b62ed5
JohnWick92 <grumec92@gmail.com>
2022-12-01 21:21:28 -0300

    Update Routes

--- api/index.ts
+++ api/index.ts
@@ -5,18 +5,11 @@ import { PostResolver } from './src/resolvers/PostResolver'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { context } from './context'
-import router from './src/Routes'
-import express from 'express'
 import path from 'path'
 import http from 'http'
-import cors from 'cors'
+import app from './app'
 
 async function startServer() {
-  const app = express()
-  app.use(express.urlencoded({ extended: true }))
-  app.use(express.json())
-  app.use(cors())
-  app.use(router)
   const httpServer = http.createServer(app)
   const schema = await buildSchema({
     resolvers: [PostResolver],
